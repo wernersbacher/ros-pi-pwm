9801753cbe8fb2a8bf519fc498c256b6a39efe34
Markus Wernersbach <m.wernersbach@hs-manheim.de>
2022-05-11 15:17:14 +0200

    message hinzugefügt, anderes gpio package für pwm hardware

--- scripts/pwm.py
+++ scripts/pwm.py
@@ -1,37 +1,41 @@
-import logging
-import time
-import RPi.GPIO as GPIO
+#!/usr/bin/python3
 
-PWM1PIN = 12  # adapt to your wiring
-PWM2PIN = 33  # adapt to your wiring
+import rospy
+#from std_msgs.msg import String
+from ros_pi_pwm.msg import command
+from rpi_hardware_pwm import HardwarePWM
+
 fPWM = 50  # Hz (not higher with software PWM)
-
 
 
 class PWM:
 
     def __init__(self):
+        self.pwm0 = None
         self.pwm1 = None
-        self.pwm2 = None
         self.setup()
 
-
     def setup(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(PWM1PIN, GPIO.OUT)
-        GPIO.setup(PWM2PIN, GPIO.OUT)
-        self.pwm1 = GPIO.PWM(PWM1PIN, fPWM)
-        self.pwm2 = GPIO.PWM(PWM2PIN, fPWM)
+        self.pwm0 = HardwarePWM(pwm_channel=0, hz=fPWM)
+        self.pwm1 = HardwarePWM(pwm_channel=1, hz=fPWM)
+        self.pwm0.start(0)
         self.pwm1.start(0)
-        self.pwm2.start(0)
 
-    def set_dc(self, duty):
-        self.pwm_engine.ChangeDutyCycle(duty)
+    def set_dc(self, pwm_num, duty):
+        """ todo: check types """
         
+        if pwm_num == 1:
+            self.pwm1.change_duty_cycle(duty)
+        else:
+            self.pwm0.change_duty_cycle(duty)
+                   
 
+pwm = PWM()
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + f"Servo num: {data.pwm_num}, DC = {data.duty}%")
+    pwm.set_dc(data.pwm_num, data.duty)
+    
 
     
 def listener():
@@ -43,11 +47,12 @@ def listener():
     # run simultaneously.
     rospy.init_node('pwm_pi_node', anonymous=True)
 
-    rospy.Subscriber("pwm1_listener", String, callback)
+    rospy.Subscriber("pwm_listener", command, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
     
 
 if __name__ == '__main__':
+    
     listener()
